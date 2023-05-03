9a370fc7b89acf8e7777519e2e38f59adfd159b8
nathaliachand <nathalia.chandra@binus.ac.id>
2023-05-03 15:03:53 +0700

    test

--- resources/js/Components/Cards.jsx
+++ resources/js/Components/Cards.jsx
@@ -27,14 +27,42 @@ const CardCarousel = () => {
     };
   
     return (
-      <Slider {...settings}>
-        {cardData.map((card) => (
-          <div key={card.id}>
-            <Card {...card} />
-          </div>
+        <div className='max-w-[1340px] mx-auto grid md:grid-cols-3 gap-8 pb-20'>
+                        {/* First */}
+                        <div className='w-full shadow-xl flex flex-col rounded-lg border border-gray-300'>
+                            <img src='images/CaraKonseling1.jpg' alt=''/>
+                            <div className='grid grid-cols-2 min-w-[900px]'>
+                                <div className='absolute pt-7 px-10'>
+                                    <NumberBox number={1} />
+                                </div>                                
+                                <h3 className='text-xl font-bold px-24 py-8'>Pilih Psikologmu</h3>
+                            </div>                            
+                            <p className='text-[#736D6D] px-10 text-[16px] pb-40 '>Pilih psikolog yang sesuai dengan permasalahanmu. Tidak perlu khawatir, psikolog kami akan senang mendengar ceritamu.</p>
+                        </div>
+                        {/* Second */}
+                        <div className='w-full shadow-xl flex flex-col rounded-lg border border-gray-300'>
+                            <img src='images/CaraKonseling2.jpg' alt=''/>
+                            <div className='grid grid-cols-2 min-w-[900px]'>
+                                <div className='absolute pt-7 px-10'>
+                                    <NumberBox number={2} />
+                                </div>                                
+                                <h3 className='text-xl font-bold px-24 py-6'>Pilih Layanan dan Jadwalkan Konsultasimu</h3>
+                            </div> 
+                            <p className='text-[#736D6D] px-10 text-[16px] pb-40 '>Pilih layanan konsultasi sesuai kebutuhanmu. Cocokkan jadwal ketersediaanmu dengan psikolog dan pastikan jadwalmu senggang.</p>
+                        </div>
+                        {/* Third */}
+                        <div className='w-full shadow-xl flex flex-col rounded-lg border border-gray-300'>
+                            <img src='images/CaraKonseling3.png' alt=''/>
+                            <div className='grid grid-cols-2 min-w-[900px]'>
+                                <div className='absolute pt-7 px-10'>
+                                    <NumberBox number={3} />
+                                </div>                                
+                                <h3 className='text-xl font-bold px-24 py-8'>Ceritakan Permasalahanmu</h3>
+                            </div> 
+                            <p className='text-[#736D6D] px-10 text-[16px] pb-40 '>Jangan khawatir bercerita dengan psikolog. Ceritakan semua permasalahanmu hingga kamu merasa lebih baik.</p>
+                        </div>
+                    </div>
         ))}
       </Slider>
     );
-  };
-  
-  
+}
