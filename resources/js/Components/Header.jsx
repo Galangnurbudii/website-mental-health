c90cdd58aba1f394811c9a84e3d67159c44a9f90
nathaliachand <nathalia.chandra@binus.ac.id>
2023-06-14 10:10:49 +0700

    new

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header({ className = "", disabled, ...props }) {
 
         {state === 2 && (
             // State 2
-            <div className="w-full bg-disabledBackground p-3 pl-10 sm:pl-16 md:pl-16 lg:pl-32">
+            <div className="w-full bg-disabledBackground p-3 pl-6 sm:pl-6 md:pl-16 lg:pl-16">
             <p className="font-semibold">
                 <span className="text-primary pr-4">
                 <Link href={route("dashboard")}>{props.pages2}</Link>
