0f91ac7b8cf4f945d3934389ae34fae4685f2496
Kinethic <chandrakusuma228@icloud.com>
2023-06-20 16:19:38 +0700

    A

--- resources/js/Pages/DashboardAdmin.jsx
+++ resources/js/Pages/DashboardAdmin.jsx
@@ -1,11 +1,8 @@
 import Footer from "@/Components/Footer1";
 import NavBar from "@/Components/NavBar";
-import React, { useState } from "react";
+import React from "react";
 
 export default function DashboardAdmin() {
-    const [showMyModal, setShowMyModal] = useState(false)
-
-    const handleOnClose = () => setShowMyModal(false)
     return (
         <>
             <NavBar />
