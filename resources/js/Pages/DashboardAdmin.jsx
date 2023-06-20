10b6972a73c8cda1568b7166e8a93fe610cd2f36
Kinethic <chandrakusuma228@icloud.com>
2023-06-20 16:19:14 +0700

    A

--- resources/js/Pages/DashboardAdmin.jsx
+++ resources/js/Pages/DashboardAdmin.jsx
@@ -1,6 +1,5 @@
-// import Footer from "@/Components/Footer1";
-// import NavBar from "@/Components/NavBar";
-import MyModal from "@/Components/MyModal";
+import Footer from "@/Components/Footer1";
+import NavBar from "@/Components/NavBar";
 import React, { useState } from "react";
 
 export default function DashboardAdmin() {
@@ -9,19 +8,13 @@ export default function DashboardAdmin() {
     const handleOnClose = () => setShowMyModal(false)
     return (
         <>
-            <div className="bg-blue-400 bg-opacity-30">
-                <div className="max-w-3xl mx-auto">
-                    <div className="text-center py-3">
-                        <button onClick={() => setShowMyModal(true)} 
-                                className="bg-red-400 text-white 
-                                            px-3 py-2 rounded hover:scale-95 
-                                            transition text-xl">
-                            Batal
-                        </button>
-                    </div>
-                </div>
-                <MyModal onClose={handleOnClose} visible={showMyModal}/>
+            <NavBar />
+
+            <div>
+                A
             </div>
+
+            <Footer />
         </>
     );
 }
