8a8160f443f8d8fb4a44b50940afc460abb11384
nurbudigalang <galangnurbudiutomo@gmail.com>
2023-07-04 00:02:07 +0700

    branch nath clear

--- resources/js/Pages/Layanan.jsx
+++ resources/js/Pages/Layanan.jsx
@@ -7,12 +7,12 @@ import CardsLayanan from "@/Components/CardsLayanan";
 export default function Layanan() {
     return (
         <div className="overflow-x-hidden">
-            <NavBarUser />
+            <NavBar />
             <Header 
                 pages = "Konsultasi"            
                 currpages = "Layanan"                
             />
-            <div className="w-full h-screen flex items-start sm:flex-col md:flex-row lg:flex-row">
+            <div className="w-full flex items-start sm:flex-col md:flex-row lg:flex-row">
                 {/* left side */}
                 <LeftSideLayanan />
                 {/* right side */}
