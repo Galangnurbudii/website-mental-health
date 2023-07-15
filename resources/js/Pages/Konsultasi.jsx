08b59c6aca87aba593d4b5af0bd337720ef6b6f4
nurbudigalang <galangnurbudiutomo@gmail.com>
2023-07-15 23:38:12 +0700

    merge

--- resources/js/Pages/Konsultasi.jsx
+++ resources/js/Pages/Konsultasi.jsx
@@ -31,32 +31,32 @@ export default function Konsultasi() {
     ]
     return (
         <div className="pt-15">
-
             <NavBar />
             <div className="bg-cardBlue w-full md:h-[80vh] mx-auto flex flex-col md:flex-row items-center md:px-16 lg:pl-32">
-            <div className="flex flex-wrap flex-col max-full p-6 md:p-0 md:w-1/2 ">
-                <div className="flex flex-col justify-center gap-2 pb-4 ">
-                    <h1 className="text-hitam font-bold text-2xl md:text-4xl">
-                        Konseling dengan Psikolog Terbaik
-                    </h1>
-                    <h2 className="text-hitam font-medium text-md md:text-2xl">
-                        Ceritakan isi hatimu, temukan akar masalah dan jadi pribadi lebih baik
-                    </h2>
+                <div className="flex flex-wrap flex-col max-full p-6 md:p-0 md:w-1/2 ">
+                    <div className="flex flex-col justify-center gap-2 pb-4 ">
+                        <h1 className="text-hitam font-bold text-2xl md:text-4xl">
+                            Konseling dengan Psikolog Terbaik
+                        </h1>
+                        <h2 className="text-hitam font-medium text-md md:text-2xl">
+                            Ceritakan isi hatimu, temukan akar masalah dan jadi
+                            pribadi lebih baik
+                        </h2>
+                    </div>
+                    <Link href={route('konsultasi')}>
+                        <PrimaryButton className="text-xs px-2 py-1 w-44 md:w-56 md:text-base">
+                            Konsultasi Sekarang
+                        </PrimaryButton>
+                    </Link>
                 </div>
-                <Link href={route("konsultasi")}>
-                    <PrimaryButton className="text-xs px-2 py-1 w-44 md:w-56 md:text-base">
-                        Konsultasi Sekarang
-                    </PrimaryButton>
-                </Link>
+                <div className="max-full md:flex md:justify-center px-4 md:w-1/2 sm:flex md:self-end">
+                    <img
+                        src="images/consultationBanner.png"
+                        alt="landingPage"
+                        className="max-h-full w-full md:pt-32"
+                    />
+                </div>
             </div>
-            <div className="max-full md:flex md:justify-center px-4 md:w-1/2 sm:flex md:self-end">
-                <img
-                    src="images/consultationBanner.png"
-                    alt="landingPage"
-                    className="max-h-full w-full md:pt-32"
-                />
-            </div>
-        </div>
 
             {/* 3 Langkah Melakukan Konseling */}
 
@@ -107,5 +107,5 @@ export default function Konsultasi() {
             </div>
             <Footer />
         </div>
-    );
+    )
 }
