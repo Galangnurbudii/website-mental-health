b8e8c56e02325874b7f527efe7c80148d887e8e1
jessicahidayat <jessica.hidayat@binus.ac.id>
2023-07-18 00:11:42 +0700

    atur ketersediaan psikolog

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\LayananController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PsikologController;
 use App\Http\Controllers\AdminArtikelController;
-
+use App\Models\Psikolog;
 
 // Auth::routes(['verify' => true]);
 
@@ -109,6 +109,8 @@ Route::get('/syaratketentuan', function () {
     return Inertia::render('SyaratDanKetentuan');
 })->name('syaratdanketentuan');
 
+Route::post('/tanggal-tidak-tersedia', [PsikologController::class, 'tanggalTakTersedia'])->name('tanggalTakTersedia');
+
 
 Route::middleware('psikolog')->group(function () {
     Route::get('/dashboardpsikolog', function () {
@@ -119,9 +121,7 @@ Route::middleware('psikolog')->group(function () {
         return Inertia::render('JadwalKonsultasi');
     })->name('jadwalkonsultasi');
 
-    Route::get('/aturketersediaan', function () {
-        return Inertia::render('AturKetersediaan');
-    })->name('aturketersediaan');
+    Route::get('/aturketersediaan', [PsikologController::class, 'getDataFromPsikolog'])->name('aturketersediaan');
 });
 
 Route::post('/callback', [LayananController::class, 'callback']);
